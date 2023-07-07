2fcc172bf01f8877266e361f781e4c5dd72ef344
Cedric D. Isubol <64195741+technobytedev@users.noreply.github.com>
2023-07-07 14:38:00 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const beshifyBtn = document.getElementById("beshifyBtn");
 beshifyBtn.addEventListener("click", beshify);
 
 function beshify(){
+    document.getElementById("alert").style.display = "none";
 
     var string = document.getElementById('phrase').value;
     var lis = string.split(" ");
@@ -21,20 +22,32 @@ const copy = document.getElementById("copy");
 copy.addEventListener("click", copyText);
 
 function copyText() {
+
+
   var copyText = document.getElementById("beshified_phrase").innerHTML;
+  
 
   navigator.clipboard.writeText(copyText)
     .then(() => {
 
         document.getElementById("alert").style.display = "";
-        sleep(2);
-        document.getElementById("alert").style.display = "none";
-
 
     })
     .catch((error) => {
       console.error("Failed to copy text: ", error);
     });
+}
+
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearText);
+
+function clearText() {
+
+
+ document.getElementById("phrase").value = '';
+  
+
 }
  
  
